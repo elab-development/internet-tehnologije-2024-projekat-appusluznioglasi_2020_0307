053971d81cdacf5dd9bd27427a463387ffecc7e7
lazar-jankovic <lazarj1007@gmail.com>
2025-10-01 14:38:39 +0200

    Dodati razliciti tipovi migracija

---
+++ database/migrations/2025_10_01_123019_add_rating_constraint_to_reviews_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('reviews', function (Blueprint $table) {
+
+            $table->decimal('rating', 3, 2)->change();
+            DB::statement('ALTER TABLE reviews ADD CONSTRAINT rating_check CHECK (rating >= 1 AND rating <= 5)');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('reviews', function (Blueprint $table) {
+            $table->tinyInteger('rating')->change();
+        });
+    }
+};
