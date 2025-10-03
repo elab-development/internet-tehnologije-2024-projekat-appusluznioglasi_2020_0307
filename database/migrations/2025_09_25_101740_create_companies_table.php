0cc1bdb9a0a87f464c7d82deec42474c6d6d36d1
lazar-jankovic <lazarj1007@gmail.com>
2025-10-03 18:34:07 +0200

    Dodat spoljni kljuc

--- database/migrations/2025_09_25_101740_create_companies_table.php
+++ database/migrations/2025_09_25_101740_create_companies_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->text('description')->nullable();
             $table->boolean('badge_verified')->default(false);
-            $table->unsignedBigInteger('user_id')->unique();
+            $table->foreignId('user_id')->constrained("users")->onDelete("cascade");
             $table->timestamps();
         });
     }
